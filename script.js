71384d3e2b9ea020000fc5f9884ea03f8428fe57
KaelSM <kaelsebastian.moreiragonzalez@student.sl.on.ca>
2023-05-21 03:48:10 -0400

    morning and night counts working

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
-function calculateResults() {
-    var spicyInput1 = parseInt(document.querySelector(".input-box-mor-spi:nth-of-type(1)").value) || 0;
-    var spicyInput2 = parseInt(document.querySelector(".input-box-mor-spi:nth-of-type(2)").value) || 0;
-    var spicyInput3 = parseInt(document.querySelector(".input-box-mor-spi:nth-of-type(3)").value) || 0;
+function calculateResults(section) {
+    // Get the containers within the specified section
+    const containers = document.querySelectorAll(`#${section} .container`);
   
-    var classicInput1 = parseInt(document.querySelector(".input-box-mor-cla:nth-of-type(1)").value) || 0;
-    var classicInput2 = parseInt(document.querySelector(".input-box-mor-cla:nth-of-type(2)").value) || 0;
-    var classicInput3 = parseInt(document.querySelector(".input-box-mor-cla:nth-of-type(3)").value) || 0;
+    // Iterate over each container
+    containers.forEach(container => {
+      // Get the input boxes within the current container
+      const inputBoxes = container.querySelectorAll('input[type="text"]');
   
-    var grillInput1 = parseInt(document.querySelector(".input-box-mor-gril:nth-of-type(1)").value) || 0;
-    var grillInput2 = parseInt(document.querySelector(".input-box-mor-gril:nth-of-type(2)").value) || 0;
-    var grillInput3 = parseInt(document.querySelector(".input-box-mor-gril:nth-of-type(3)").value) || 0;
+      // Initialize the result variable
+      let result = 0;
   
-    var spicyResult = spicyInput1 * 100 + spicyInput2 * 10 + spicyInput3;
-    var classicResult = classicInput1 * 100 + classicInput2 * 10 + classicInput3;
-    var grillResult = grillInput1 * 100 + grillInput2 * 10 + grillInput3;
+      // Calculate the result
+      inputBoxes.forEach(inputBox => {
+        const value = parseInt(inputBox.value);
+        if (!isNaN(value)) {
+          const label = inputBox.nextElementSibling.innerText;
+          const multiplier = parseInt(label.substring(1));
+          result += value * multiplier;
+        }
+      });
   
-    document.querySelector(".result-text-mor-spi").textContent = spicyResult;
-    document.querySelector(".result-text-mor-cla").textContent = classicResult;
-    document.querySelector(".result-text-mor-gril").textContent = grillResult;
+      // Update the result text within the current container
+      const resultText = container.querySelector('.result-text');
+      resultText.textContent = result;
+    });
   }
   

77e719c285425be8ec01daabcd938a0f51606f31
KaelSM <kaelsebastian.moreiragonzalez@student.sl.on.ca>
2023-05-21 21:41:35 -0400

    the mid finally working

--- script.js
+++ script.js
@@ -26,92 +26,166 @@ function calculateResult(section) {
   });
 }
 
+let onlineCalculated = false;
+let freezerCalculated = false;
+
 function calculateResults(section) {
-  // Get the containers within the specified section
-  const containers = document.querySelectorAll(`#${section} .container`);
-
-  // Initialize the section result variable
-  let sectionResult = 0;
-
-  // Iterate over each container
-  containers.forEach(container => {
-    // Get the input boxes within the current container
-    const inputBoxes = container.querySelectorAll('input[type="text"]');
-
-    // Initialize the container result variable
-    let result = 0;
-
-    // Calculate the container result
-    inputBoxes.forEach(inputBox => {
-      const value = parseInt(inputBox.value);
-      if (!isNaN(value)) {
-        const label = inputBox.nextElementSibling.innerText;
-        const multiplier = parseInt(label.substring(1));
-        result += value * multiplier;
-      }
-    });
-
-    // Update the result text within the current container
-    const resultText = container.querySelector('.result-text');
-    if (resultText) {
-      resultText.textContent = result;
-    }
-
-    // Add the container result to the section result
-    sectionResult += result;
-  });
-
-  // Update the total result text for the section
-  const totalResultText = document.querySelector(`.${section}-total .result-text`);
-  if (totalResultText) {
-    totalResultText.textContent = sectionResult;
+  if (section === 'On-line') {
+    onlineCalculated = true;
+  } else if (section === 'Freezer') {
+    freezerCalculated = true;
   }
 
-  // Calculate the total for Online and Freezer
-  const onlineContainers = document.querySelectorAll('#On-line .container');
-  const freezerContainers = document.querySelectorAll('#Freezer .container');
-  let onlineResult = 0;
-  let freezerResult = 0;
-
-  // Calculate the Online result
-  onlineContainers.forEach(container => {
-    const inputBoxes = container.querySelectorAll('input[type="text"]');
-    let result = 0;
-    inputBoxes.forEach(inputBox => {
-      const value = parseInt(inputBox.value);
-      if (!isNaN(value)) {
-        const label = inputBox.nextElementSibling.innerText;
-        const multiplier = parseInt(label.substring(1));
-        result += value * multiplier;
-      }
-    });
-    onlineResult += result;
-  });
-
-  // Calculate the Freezer result
-  freezerContainers.forEach(container => {
-    const inputBoxes = container.querySelectorAll('input[type="text"]');
-    let result = 0;
-    inputBoxes.forEach(inputBox => {
-      const value = parseInt(inputBox.value);
-      if (!isNaN(value)) {
-        const label = inputBox.nextElementSibling.innerText;
-        const multiplier = parseInt(label.substring(1));
-        result += value * multiplier;
-      }
-    });
-    freezerResult += result;
-  });
-
-  // Update the total result text for Online and Freezer
-  const onlineTotalResultText = document.querySelector('.On-line-total .result-text');
-  const freezerTotalResultText = document.querySelector('.Freezer-total .result-text');
-  if (onlineTotalResultText && freezerTotalResultText) {
-    const onlineFreezerTotalResult = onlineResult + freezerResult;
-    onlineTotalResultText.textContent = onlineResult;
-    freezerTotalResultText.textContent = freezerResult;
+  if (onlineCalculated && freezerCalculated) {
+    showTotalResults();
   }
 }
 
+function showTotalResults() {
+  // Get the container elements for both sections
+  const onlineContainer = document.getElementById("On-line");
+  const freezerContainer = document.getElementById("Freezer");
 
+  // Calculate and show the results for each chicken type in the "On-line" section
+  const onlineSpicyTotal = calculateTotal(onlineContainer, '.spicy.container');
+  const onlineClassicTotal = calculateTotal(onlineContainer, '.classic.container');
+  const onlineGrillTotal = calculateTotal(onlineContainer, '.grill.container');
+  const onlineNuggetTotal = calculateTotal(onlineContainer, '.nugget.container');
+  const onlineStripsTotal = calculateTotal(onlineContainer, '.strips.container');
+
+  updateResult(onlineContainer, '.spicy.container', onlineSpicyTotal);
+  updateResult(onlineContainer, '.classic.container', onlineClassicTotal);
+  updateResult(onlineContainer, '.grill.container', onlineGrillTotal);
+  updateResult(onlineContainer, '.nugget.container', onlineNuggetTotal);
+  updateResult(onlineContainer, '.strips.container', onlineStripsTotal);
+
+  // Calculate and show the results for each chicken type in the "Freezer" section
+  const freezerSpicyTotal = calculateTotal(freezerContainer, '.spicy.container');
+  const freezerClassicTotal = calculateTotal(freezerContainer, '.classic.container');
+  const freezerGrillTotal = calculateTotal(freezerContainer, '.grill.container');
+  const freezerNuggetTotal = calculateTotal(freezerContainer, '.nugget.container');
+  const freezerStripsTotal = calculateTotal(freezerContainer, '.strips.container');
+
+  updateResult(freezerContainer, '.spicy.container', freezerSpicyTotal);
+  updateResult(freezerContainer, '.classic.container', freezerClassicTotal);
+  updateResult(freezerContainer, '.grill.container', freezerGrillTotal);
+  updateResult(freezerContainer, '.nugget.container', freezerNuggetTotal);
+  updateResult(freezerContainer, '.strips.container', freezerStripsTotal);
+
+  // Update the total container with the calculated totals
+  const totalSpicy = document.querySelector('.total.spicy-total .result-text');
+  const totalClassic = document.querySelector('.total.classic-total .result-text');
+  const totalGrill = document.querySelector('.total.grill-total .result-text');
+  const totalNugget = document.querySelector('.total.nugget-total .result-text');
+  const totalStrips = document.querySelector('.total.strips-total .result-text');
+
+  // Calculate the sum of totals from both sections
+  const spicyTotal = onlineSpicyTotal + freezerSpicyTotal;
+  const classicTotal = onlineClassicTotal + freezerClassicTotal;
+  const grillTotal = onlineGrillTotal + freezerGrillTotal;
+  const nuggetTotal = onlineNuggetTotal + freezerNuggetTotal;
+  const stripsTotal = onlineStripsTotal + freezerStripsTotal;
+
+  // Update the total result container with the sum of totals
+  totalSpicy.textContent = spicyTotal;
+  totalClassic.textContent = classicTotal;
+  totalGrill.textContent = grillTotal;
+  totalNugget.textContent = nuggetTotal;
+  totalStrips.textContent = stripsTotal;
+
+  // Show the total result container
+  const totalResultContainer = document.querySelector('.total-result-container');
+  totalResultContainer.style.display = 'block';
+}
+
+function calculateResults(section) {
+  const containers = document.querySelectorAll(`#${section} .container`);
+
+  let total = 0;
+
+  containers.forEach((container) => {
+    const inputBoxes = container.querySelectorAll('.input-box-aft');
+    const resultText = container.querySelector('.result-text');
+    let subTotal = 0;
+
+    inputBoxes.forEach((inputBox) => {
+      const value = parseInt(inputBox.value);
+      const multiplier = parseInt(inputBox.nextElementSibling.innerText.replace('x', ''));
+      subTotal += value * multiplier;
+    });
+
+    resultText.innerText = subTotal;
+    total += subTotal;
+  });
+
+  showTotalResults(total);
+}
+
+
+function showTotalResults() {
+  const totalContainers = document.querySelectorAll('.total-container .total');
+
+  let spicyTotal = 0;
+  let classicTotal = 0;
+  let grillTotal = 0;
+  let nuggetTotal = 0;
+  let stripsTotal = 0;
+
+  const onlineContainers = document.querySelectorAll('#On-line .container');
+  onlineContainers.forEach((container) => {
+    const resultText = container.querySelector('.result-text');
+    const header = container.querySelector('.header').innerText;
+    const inputBoxes = container.querySelectorAll('.input-box-aft');
+    const inputValues = Array.from(inputBoxes).map((input) => parseInt(input.value));
+
+    if (header === 'Spicy') {
+      spicyTotal += inputValues[0] * 10 + inputValues[1] * 1;
+    } else if (header === 'Classic') {
+      classicTotal += inputValues[0] * 10 + inputValues[1] * 1;
+    } else if (header === 'Grill') {
+      grillTotal += inputValues[0] * 10 + inputValues[1] * 1;
+    } else if (header === 'Nuggets') {
+      nuggetTotal += inputValues[0] * 38 + inputValues[1] * 1;
+    } else if (header === 'Strips') {
+      stripsTotal += inputValues[0] * 18 + inputValues[1] * 1;
+    }
+  });
+
+  const freezerContainers = document.querySelectorAll('#Freezer .container');
+  freezerContainers.forEach((container) => {
+    const resultText = container.querySelector('.result-text');
+    const header = container.querySelector('.header').innerText;
+    const inputBoxes = container.querySelectorAll('.input-box-aft');
+    const inputValues = Array.from(inputBoxes).map((input) => parseInt(input.value));
+
+    if (header === 'Spicy') {
+      spicyTotal += inputValues[0] * 100 + inputValues[1] * 10;
+    } else if (header === 'Classic') {
+      classicTotal += inputValues[0] * 100 + inputValues[1] * 10;
+    } else if (header === 'Grill') {
+      grillTotal += inputValues[0] * 100 + inputValues[1] * 10;
+    } else if (header === 'Nuggets') {
+      nuggetTotal += inputValues[0] * 760 + inputValues[1] * 38;
+    } else if (header === 'Strips') {
+      stripsTotal += inputValues[0] * 252 + inputValues[1] * 18;
+    }
+  });
+
+  totalContainers.forEach((totalContainer) => {
+    const resultText = totalContainer.querySelector('.result-text');
+    const header = totalContainer.querySelector('.header').innerText;
+
+    if (header === 'Spicy') {
+      resultText.innerText = spicyTotal.toFixed(2);
+    } else if (header === 'Classic') {
+      resultText.innerText = classicTotal.toFixed(2);
+    } else if (header === 'Grill') {
+      resultText.innerText = grillTotal.toFixed(2);
+    } else if (header === 'Nuggets') {
+      resultText.innerText = nuggetTotal.toFixed(2);
+    } else if (header === 'Strips') {
+      resultText.innerText = stripsTotal.toFixed(2);
+    }
+  });
+}
 
